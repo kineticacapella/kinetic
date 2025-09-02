e01d84fda383cb282d6d0ff1813e92f4c539701d
kineticacapella <my.lib.1025@proton.me>
2025-09-02 16:00:02 +0000

    border colour consistently

--- kinetic/src/lib/components/WorkoutsTab.svelte
+++ kinetic/src/lib/components/WorkoutsTab.svelte
@@ -778,7 +778,7 @@
 	class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
 >
 	<div class="relative p-4 w-full max-w-2xl max-h-full">
-		<div class="relative bg-white rounded-lg shadow-xl dark:bg-gray-800 border-2 border-blue-500 dark:border-blue-400">
+		<div class="relative bg-white rounded-lg shadow-xl dark:bg-gray-800 border-2 border-blue-700 dark:border-blue-600">
 			<div
 				class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
 			>
