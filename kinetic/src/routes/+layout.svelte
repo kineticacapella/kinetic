2ff2d7b8bba649b11faca2154fab3766b0d8ca7a
kineticacapella <my.lib.1025@proton.me>
2025-09-10 15:46:41 +0000

    Settings tab icon

--- kinetic/src/routes/+layout.svelte
+++ kinetic/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { UserSettingsSolid } from 'flowbite-svelte-icons';
 	import '../app.css';
 	import ExercisesTab from '$lib/components/ExercisesTab.svelte';
 	import SettingsTab from '$lib/components/SettingsTab.svelte';
@@ -15,7 +16,7 @@
 <div class="bg-gray-50 dark:bg-gray-900 min-h-screen flex flex-col">
 	<main class="flex-grow container mx-auto p-4">
 		<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
-			<ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+			<ul class="flex flex-wrap -mb-px text-sm font-medium" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
 				<li class="mr-2" role="presentation">
 					<button class="inline-block p-4 border-b-2 rounded-t-lg" id="workouts-tab" data-tabs-target="#workouts" type="button" role="tab" aria-controls="workouts" aria-selected="true">Workouts</button>
 				</li>
@@ -25,8 +26,10 @@
 				<li class="mr-2" role="presentation">
 					<button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="history-tab" data-tabs-target="#history" type="button" role="tab" aria-controls="history" aria-selected="false">History</button>
 				</li>
-				<li class="mr-2" role="presentation">
-					<button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">Settings</button>
+				<li class="ml-auto" role="presentation">
+					<button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="settings-tab" data-tabs-target="#settings" type="button" role="tab" aria-controls="settings" aria-selected="false">
+						<UserSettingsSolid class="shrink-0 h-6 w-6" />
+					</button>
 				</li>
 			</ul>
 		</div>
