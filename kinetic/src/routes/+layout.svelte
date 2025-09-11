60e0ec1940ece614fbb02c141c63635f4b45f66d
kineticacapella <my.lib.1025@proton.me>
2025-09-11 17:05:08 +0000

    3rd floating menu attempt

--- kinetic/src/routes/+layout.svelte
+++ kinetic/src/routes/+layout.svelte
@@ -17,15 +17,15 @@
 <div class="bg-gray-50 dark:bg-gray-900 min-h-screen flex flex-col">
 	<main class="flex-grow container mx-auto p-4">
 		<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
-			<ul class="flex flex-wrap -mb-px text-sm font-medium" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
+			<ul class="flex flex-wrap -mb-px text-sm font-medium" role="tablist">
 				<li class="mr-2" role="presentation">
-					<button class="inline-block p-4 border-b-2 rounded-t-lg" id="workouts-tab" data-tabs-target="#workouts" type="button" role="tab" aria-controls="workouts" aria-selected="true">Workouts</button>
+					<div class="inline-block p-4 border-b-2 rounded-t-lg invisible">Workouts</div>
 				</li>
 				<li class="mr-2" role="presentation">
-					<button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="exercises-tab" data-tabs-target="#exercises" type="button" role="tab" aria-controls="exercises" aria-selected="false">Exercises</button>
+					<div class="inline-block p-4 border-b-2 rounded-t-lg invisible">Exercises</div>
 				</li>
 				<li class="mr-2" role="presentation">
-					<button class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="history-tab" data-tabs-target="#history" type="button" role="tab" aria-controls="history" aria-selected="false">History</button>
+					<div class="inline-block p-4 border-b-2 rounded-t-lg invisible">History</div>
 				</li>
 				<li class="ml-auto flex items-center" role="presentation">
 					<div class="mr-2">
@@ -44,6 +44,19 @@
 					</button>
 				</li>
 			</ul>
+		</div>
+		<div class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+			<div class="grid h-full max-w-lg grid-cols-3 mx-auto font-medium" id="myTab" data-tabs-toggle="#myTabContent">
+				<button class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" id="workouts-tab" data-tabs-target="#workouts" type="button" role="tab" aria-controls="workouts" aria-selected="true">
+					Workouts
+				</button>
+				<button class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" id="exercises-tab" data-tabs-target="#exercises" type="button" role="tab" aria-controls="exercises" aria-selected="false">
+					Exercises
+				</button>
+				<button class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group" id="history-tab" data-tabs-target="#history" type="button" role="tab" aria-controls="history" aria-selected="false">
+					History
+				</button>
+			</div>
 		</div>
 		<div id="myTabContent">
 			<div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="workouts" role="tabpanel" aria-labelledby="workouts-tab">
