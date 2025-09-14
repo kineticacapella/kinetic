15ad2b16a6edd781351ac22d45efab32e13c8926
kineticacapella <my.lib.1025@proton.me>
2025-09-14 12:04:03 +0000

    Styling for exercise picking modal fixed

--- kinetic/src/lib/components/WorkoutsTab.svelte
+++ kinetic/src/lib/components/WorkoutsTab.svelte
@@ -1137,12 +1137,12 @@
 	class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
 >
 	<div class="relative p-4 w-full max-w-md max-h-full">
-		<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+		<div class="relative bg-white rounded-lg shadow-xl dark:bg-gray-800 border-2 border-blue-700 dark:border-blue-600">
 			<div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
 				<h3 class="text-lg font-semibold text-gray-900 dark:text-white">Add Exercise</h3>
 				<button
 					type="button"
-					class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+					class="text-red-500 bg-transparent hover:bg-red-100 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:text-red-400 dark:hover:bg-red-900"
 					data-modal-hide="add-exercise-modal"
 				>
 					<svg
