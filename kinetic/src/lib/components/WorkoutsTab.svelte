aa799292ef4181396c11b3c461be89e0450c2b1e
kineticacapella <my.lib.1025@proton.me>
2025-09-14 16:24:28 +0000

    Fixed text alignment in 'Log' column

--- kinetic/src/lib/components/WorkoutsTab.svelte
+++ kinetic/src/lib/components/WorkoutsTab.svelte
@@ -962,7 +962,7 @@
 												class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
 											/>
 										</div>
-										<div class="flex justify-center">
+										<div class="text-center">
 											<input
 												type="checkbox"
 												class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 disabled:opacity-50 disabled:cursor-not-allowed"
