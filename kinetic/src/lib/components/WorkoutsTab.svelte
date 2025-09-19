4766c4fb8f14d51cf4fa55870436cf4cd7fe4e28
kineticacapella <my.lib.1025@proton.me>
2025-09-19 16:27:56 +0000

    reps field opacity

--- kinetic/src/lib/components/WorkoutsTab.svelte
+++ kinetic/src/lib/components/WorkoutsTab.svelte
@@ -976,7 +976,7 @@
 												type="number"
 												id="reps-{set.id}"
 												bind:value={set.reps}
-												class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+												class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white {workoutMode === 'play' ? 'opacity-50' : ''}"
 											/>
 										</div>
 										<div class="flex justify-center">
