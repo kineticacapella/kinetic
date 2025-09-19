1ce9018ce3aa5e44b844102766ed4cb060e4bf0f
kineticacapella <my.lib.1025@proton.me>
2025-09-19 17:01:21 +0000

    check box size

--- kinetic/src/lib/components/WorkoutsTab.svelte
+++ kinetic/src/lib/components/WorkoutsTab.svelte
@@ -990,7 +990,7 @@
 										<div class="flex justify-center">
 											<input
 												type="checkbox"
-												class="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 disabled:opacity-50 disabled:cursor-not-allowed"
+												class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 disabled:opacity-50 disabled:cursor-not-allowed"
 												disabled={workoutMode === 'edit' || isSetLogged(set.id)}
 												checked={isSetLogged(set.id)}
 												onchange={async (e) => {
