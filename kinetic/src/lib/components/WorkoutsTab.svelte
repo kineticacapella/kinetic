5e044bfd1751d004f40a7d861fe7add5009b5b2b
kineticacapella <my.lib.1025@proton.me>
2025-09-27 14:46:52 +0000

    workout card height width update

--- kinetic/src/lib/components/WorkoutsTab.svelte
+++ kinetic/src/lib/components/WorkoutsTab.svelte
@@ -710,7 +710,7 @@
 				{@const numExercises = new Set((workout.workout_exercises || []).map((e) => e.exercise_id)).size}
 				{@const numSets = (workout.workout_exercises || []).length}
 				<div
-					class="relative flex flex-col bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden transition-shadow hover:shadow-lg border-2 border-blue-700 dark:border-blue-600 self-start"
+					class="relative flex flex-col bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden transition-shadow hover:shadow-lg border-2 border-blue-700 dark:border-blue-600 self-start w-96 h-64"
 				>
 					<div class="p-6">
 						<div class="flex justify-between items-center mb-3">
