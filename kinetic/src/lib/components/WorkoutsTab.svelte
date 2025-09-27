a5caa24a538425905c174e0731e4769f272bab1e
kineticacapella <my.lib.1025@proton.me>
2025-09-27 14:54:29 +0000

    fix

--- kinetic/src/lib/components/WorkoutsTab.svelte
+++ kinetic/src/lib/components/WorkoutsTab.svelte
@@ -712,7 +712,7 @@
 				<div
 					class="relative flex flex-col bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden transition-shadow hover:shadow-lg border-2 border-blue-700 dark:border-blue-600 self-start w-96 h-64"
 				>
-					<div class="p-6">
+					<div class="p-6 flex-grow">
 						<div class="flex justify-between items-center mb-3">
 							<h5 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
 								{workout.name}
